9fe171e214e02f0ba3c75980c07746b9fa985af4
adamsjlcode <adamsjlcode@gmail.com>
2021-03-17 11:26:14 -0500

    Created Navbar Added Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import ProjectList from "./components/project-list.component";
+import EditProject from "./components/edit-project.component";
+import CreateProject from "./components/create-project.component";
+import CreateUser from "./components/create-user.component";
+
 
 function App() {
   return (
-    <div className="container">
-      Hello World
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ProjectList} />
+      <Route path="/edit/:id" exact component={EditProject} />
+      <Route path="/create" exact component={CreateProject} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 

ded00bcb01ea392b98112e1a58542580100a1c03
adamsjlcode <adamsjlcode@gmail.com>
2021-03-18 17:10:20 -0500

    Format

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,65 +1,65 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
     super(props);
 
     this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeFirstName = this.onChangeFirstName.bind(this);        
-    this.onChangeLastName = this.onChangeLastName.bind(this);      
-    this.onChangeEmail = this.onChangeEmail.bind(this);      
-    this.onChangePhone = this.onChangePhone.bind(this);        
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePhone = this.onChangePhone.bind(this);
     this.onChangeAddress = this.onChangeAddress.bind(this);
     this.onChangeUrl = this.onChangeUrl.bind(this);
-    
+
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-      username: '',
-      first_name: '',
-      last_name: '',
-      email: '',
-      phone: '',
-      address: '',
-      url: '',
-    }
+      username: "",
+      first_name: "",
+      last_name: "",
+      email: "",
+      phone: "",
+      address: "",
+      url: "",
+    };
   }
 
   onChangeUsername(e) {
     this.setState({
-      username: e.target.value
-    })
+      username: e.target.value,
+    });
   }
   onChangeFirstName(e) {
     this.setState({
-      first_name: e.target.value
-    })
+      first_name: e.target.value,
+    });
   }
   onChangeLastName(e) {
     this.setState({
-      last_name: e.target.value
-    })
+      last_name: e.target.value,
+    });
   }
   onChangeEmail(e) {
     this.setState({
-      email: e.target.value
-    })
+      email: e.target.value,
+    });
   }
   onChangePhone(e) {
     this.setState({
-      phone: e.target.value
-    })
+      phone: e.target.value,
+    });
   }
   onChangeAddress(e) {
     this.setState({
-      address: e.target.value
-    })
+      address: e.target.value,
+    });
   }
   onChangeUrl(e) {
     this.setState({
-      url: e.target.value
-    })
+      url: e.target.value,
+    });
   }
 
   onSubmit(e) {
@@ -73,23 +73,23 @@ export default class CreateUser extends Component {
       phone: this.state.phone,
       address: this.state.address,
       url: this.state.url,
-    }
+    };
 
     console.log(user);
 
-    axios.post('http://localhost:5000/users/add', user)
-      .then(res => console.log(res.data));
+    axios
+      .post("http://localhost:5000/users/add", user)
+      .then((res) => console.log(res.data));
 
     this.setState({
-      username: '',
-      first_name: '',
-      last_name: '',
-      email: '',
-      phone: '',
-      address: '',
-      url: '',
-      
-    })
+      username: "",
+      first_name: "",
+      last_name: "",
+      email: "",
+      phone: "",
+      address: "",
+      url: "",
+    });
   }
 
   render() {
@@ -97,55 +97,65 @@ export default class CreateUser extends Component {
       <div>
         <h3>Create New User</h3>
         <form onSubmit={this.onSubmit}>
-          <div className="form-group"> 
+          <div className="form-group">
             <label>Username: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
-                />
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
             <label>First Name: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.first_name}
-                onChange={this.onChangeFirstName}
-                />
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.first_name}
+              onChange={this.onChangeFirstName}
+            />
             <label>Last Name: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.last_name}
-                onChange={this.onChangeLastName}
-                />
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.last_name}
+              onChange={this.onChangeLastName}
+            />
             <label>Email: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.email}
-                onChange={this.onChangeEmail}
-                />
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+            />
             <label>Phone: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.phone}
-                onChange={this.onChangePhone}
-                />
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.phone}
+              onChange={this.onChangePhone}
+            />
             <label>Url: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.url}
-                onChange={this.onChangeUrl}
-                />
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.url}
+              onChange={this.onChangeUrl}
+            />
           </div>
           <div className="form-group">
-            <input type="submit" value="Create User" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
           </div>
         </form>
       </div>
-    )
+    );
   }
 }

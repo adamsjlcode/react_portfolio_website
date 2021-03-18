66ed140f39f5382aabbea1235f1ef9efeb40fc1e
adamsjlcode <adamsjlcode@gmail.com>
2021-03-18 17:08:32 -0500

    Format

--- server/models/project.model.js
+++ server/models/project.model.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose');
-require('mongoose-type-url');
-
+const mongoose = require("mongoose");
+const languageSchema = require("./language.model").schema;
+const urlSchema = require("./url.model").schema;
 
 const Schema = mongoose.Schema;
 
-const projectSchema = new Schema({
-    username: { type: String, required: true, },
+const projectSchema = new Schema(
+  {
+    username: { type: String, required: true },
     title: { type: String, required: true },
-    languages: [String],
-    date: { type: Date, required: true, },
-    description: { type: String, required: true, },
-    url: { github: mongoose.SchemaTypes.Url },
-}, {
+    languages: [languageSchema],
+    date: { type: Date, required: true },
+    description: { type: String, required: true },
+    urls: [urlSchema],
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Project = mongoose.model('Project', projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 
 module.exports = Project;

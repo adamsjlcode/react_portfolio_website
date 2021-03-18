34ee018d83a25e0fc20692e1259054e565bc5a88
adamsjlcode <adamsjlcode@gmail.com>
2021-03-18 17:15:29 -0500

    Format File & Updated Form

--- src/components/edit-project.component.js
+++ src/components/edit-project.component.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import DatePicker from 'react-datepicker';
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class EditProject extends Component {
@@ -8,70 +8,89 @@ export default class EditProject extends Component {
     super(props);
 
     this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeLanguages = this.onChangeLanguages.bind(this);
     this.onChangeDate = this.onChangeDate.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeUrl = this.onChangeUrl.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-      username: '',
-      description: '',
-      duration: 0,
+      username: "",
+      title: "",
+      languages: "",
       date: new Date(),
-      users: []
-    }
+      description: "",
+      url: "",
+      users: [],
+    };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/projects/'+this.props.match.params.id)
-      .then(response => {
+    axios
+      .get("http://localhost:5000/projects/" + this.props.match.params.id)
+      .then((response) => {
         this.setState({
           username: response.data.username,
+          title: response.data.title,
+          languages: response.data.languages,
+          date: new Date(response.data.date),
           description: response.data.description,
-          duration: response.data.duration,
-          date: new Date(response.data.date)
-        })   
+          url: response.data.url,
+        });
       })
       .catch(function (error) {
         console.log(error);
-      })
+      });
 
-    axios.get('http://localhost:5000/users/')
-      .then(response => {
+    axios
+      .get("http://localhost:5000/users/")
+      .then((response) => {
         if (response.data.length > 0) {
           this.setState({
-            users: response.data.map(user => user.username),
-          })
+            users: response.data.map((user) => user.username),
+          });
         }
       })
       .catch((error) => {
         console.log(error);
-      })
-
+      });
   }
 
   onChangeUsername(e) {
     this.setState({
-      username: e.target.value
-    })
+      username: e.target.value,
+    });
   }
 
-  onChangeDescription(e) {
+  onChangeTitle(e) {
     this.setState({
-      description: e.target.value
-    })
+      title: e.target.value,
+    });
   }
 
-  onChangeDuration(e) {
+  onChangeLanguages(e) {
     this.setState({
-      duration: e.target.value
-    })
+      languages: e.target.value,
+    });
   }
 
   onChangeDate(date) {
     this.setState({
-      date: date
-    })
+      date: date,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+
+  onChangeUrl(e) {
+    this.setState({
+      url: e.target.value,
+    });
   }
 
   onSubmit(e) {
@@ -79,74 +98,105 @@ export default class EditProject extends Component {
 
     const project = {
       username: this.state.username,
+      title: this.state.title,
+      languages: this.state.languages,
+      date: this.state.date,
       description: this.state.description,
-      duration: this.state.duration,
-      date: this.state.date
-    }
+      url: this.state.url,
+    };
 
     console.log(project);
 
-    axios.post('http://localhost:5000/projects/update/' + this.props.match.params.id, project)
-      .then(res => console.log(res.data));
+    axios
+      .post(
+        "http://localhost:5000/projects/update/" + this.props.match.params.id,
+        project
+      )
+      .then((res) => console.log(res.data));
 
-    window.location = '/';
+    window.location = "/";
   }
 
   render() {
     return (
-    <div>
-      <h3>Edit Project Log</h3>
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group"> 
-          <label>Username: </label>
-          <select ref="userInput"
+      <div>
+        <h3>Edit Project</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
               required
               className="form-control"
               value={this.state.username}
-              onChange={this.onChangeUsername}>
-              {
-                this.state.users.map(function(user) {
-                  return <option 
-                    key={user}
-                    value={user}>{user}
-                    </option>;
-                })
-              }
-          </select>
-        </div>
-        <div className="form-group"> 
-          <label>Description: </label>
-          <input  type="text"
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function (user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Title: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.title}
+              onChange={() => this.onChangeTitle}
+            />
+          </div>
+          <div className="form-group">
+            <label>Languages: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.languages}
+              onChange={() => this.onChangeLanguages}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={() => this.onChangeDate}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
               required
               className="form-control"
               value={this.state.description}
-              onChange={this.onChangeDescription}
-              />
-        </div>
-        <div className="form-group">
-          <label>Duration (in minutes): </label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.duration}
-              onChange={this.onChangeDuration}
-              />
-        </div>
-        <div className="form-group">
-          <label>Date: </label>
-          <div>
-            <DatePicker
-              selected={this.state.date}
-              onChange={this.onChangeDate}
+              onChange={() => this.onChangeDescription}
             />
           </div>
-        </div>
-
-        <div className="form-group">
-          <input type="submit" value="Edit Project Log" className="btn btn-primary" />
-        </div>
-      </form>
-    </div>
-    )
+          <div className="form-group">
+            <label>Url: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.url}
+              onChange={() => this.onChangeUrl}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Project"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }

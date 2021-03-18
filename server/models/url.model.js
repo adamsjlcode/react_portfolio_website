e32c11171e2fadd13e052ded33e87217f6a701db
adamsjlcode <adamsjlcode@gmail.com>
2021-03-18 17:04:42 -0500

    Add Url Model

---
+++ server/models/url.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const imageSchema = require("./image.model").schema;
+const { Schema, validator } = mongoose;
+
+const urlSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: "URL can't be empty",
+      unique: true,
+    },
+    site: {
+      type: String,
+      required: "Site can't be empty",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Url = mongoose.model("Url", urlSchema);
+
+module.exports = Url;
+// logo: imageSchema,
+// validate: {
+//     validator: (value) =>
+//       validator.isURL(value, {
+//         protocols: ["http", "https", "ftp"],
+//         require_tld: true,
+//         require_protocol: true,
+//       }),
+//     message: "Must be a Valid URL",

a3c81e081684cdcbfa1ff43e59c295e1e69db8c6
adamsjlcode <adamsjlcode@gmail.com>
2021-03-18 17:08:01 -0500

    Format

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,51 +1,63 @@
-const router = require('express').Router();
-let Project = require('../models/project.model');
+const router = require("express").Router();
+let Project = require("../models/project.model");
 
-router.route('/').get((req, res) => {
-    Project.find()
-    .then(project => res.json(project))
-    .catch(err => res.status(400).json('Error: ' + err ));
+router.route("/").get((req, res) => {
+  Project.find()
+    .then((project) => res.json(project))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const title = req.body.title;
-    const languages = req.body.languages;
-    const date = req.body.date;
-    const description = req.body.description;
-    const url = req.body.url;
-    const newProject = new Project({username, title, languages, date, description, url});
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const title = req.body.title;
+  const languages = req.body.languages;
+  const date = req.body.date;
+  const description = req.body.description;
+  const urls = req.body.urls;
 
-    newProject.save()
-    .then(users => res.json('New Project Added'))
-    .catch(err => res.status(400).json('Error: ' + err ));
-});
+  const newProject = new Project({
+    username,
+    title,
+    languages,
+    date,
+    description,
+    urls,
+  });
 
-router.route('/:id').get((req, res) => {
-    Project.findById(req.params.id)
-    .then(project => res.json(project))
-    .catch(err => res.status(400).json('Error: ' + err ));
-});
-
-router.route('/:id').delete((req, res) => {
-    Project.findByIdAndDelete(req.params.id)
-    .then(project => res.json('Project Deleted'))
-    .catch(err => res.status(400).json('Error: ' + err ));
-});
-
-router.route('/update/:id').post((req, res) => {
-    Project.findById(req.params.id)
-    .then(project => { 
-        project.username = req.body.username;
-        project.description = req.body.description;
-        project.duration = Number(req.body.duration);
-        project.date = Date.parse(req.body.date);
-
-        project.save()
-        .then(() => res.json('Project Updated'))
-        .catch(err => res.status(400).json('Error: ' + err ));
+  newProject
+    .save()
+    .then(() => {
+      res.json("New Project Added");
     })
-    .catch(err => res.status(400).json('Error: ' + err ));
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Project.findById(req.params.id)
+    .then((project) => res.json(project))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Project.findByIdAndDelete(req.params.id)
+    .then((project) => res.json("Project Deleted"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Project.findById(req.params.id)
+    .then((project) => {
+      project.username = req.body.username;
+      project.description = req.body.description;
+      project.duration = Number(req.body.duration);
+      project.date = Date.parse(req.body.date);
+
+      project
+        .save()
+        .then(() => res.json("Project Updated"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;

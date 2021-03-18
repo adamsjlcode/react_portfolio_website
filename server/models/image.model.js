4c1d064ab63b03244336527d0769349a60f82e4c
adamsjlcode <adamsjlcode@gmail.com>
2021-03-18 17:03:51 -0500

    Add Image Model

---
+++ server/models/image.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const imageSchema = new Schema(
+  {
+    image: { data: Buffer, contentType: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Image = mongoose.model("Image", imageSchema);
+
+module.exports = Image;
